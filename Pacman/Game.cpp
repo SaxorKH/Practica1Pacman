f7801cfa05aa41b854b4d31c837d6c9f88b1cc48
SaxorKH <digicarlos@live.com>
2017-11-04 20:57:38 +0100

    Game y GameObject como clases

--- Pacman/Game.cpp
+++ Pacman/Game.cpp
@@ -10,3 +10,19 @@ Game::Game()
 Game::~Game()
 {
 }
+
+void Game::run()
+{
+}
+
+void Game::render()
+{
+}
+
+void Game::update()
+{
+}
+
+void Game::handleEvents()
+{
+}
--- Pacman/Game.h
+++ Pacman/Game.h
@@ -1,13 +1,26 @@
 #pragma once
 #include "SDL.h"
+#include <vector>
+#include "GameObject.h"
+using namespace std;
 
 class Game
 {
 private:
 	SDL_Window* window = nullptr;
+	SDL_Renderer* renderer = nullptr;
+	unsigned int winWidth = 800;
+	unsigned int winHeight = 600;
+	bool exit = false;
+	vector<GameObject*> gameObjects;
 
 public:
 	Game();
 	~Game();
+
+	void run();
+	void render();
+	void update();
+	void handleEvents();
 };
 
