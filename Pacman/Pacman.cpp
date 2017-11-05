4a3024599323abfd7a07df8df60651f96c3ace1a
LluisCS <lluiscan@ucm.es>
2017-11-05 15:02:39 +0100

    Creacion Pacman

--- Pacman/Pacman.cpp
+++ Pacman/Pacman.cpp
@@ -2,11 +2,23 @@
 
 
 
-Pacman::Pacman()
+Pacman::Pacman(Game* g, Texture* t)
 {
+	game = g;
+	texture = t;
 }
 
 
 Pacman::~Pacman()
 {
+}
+
+void Pacman::setPos(unsigned int row, unsigned int col) {
+	inix = x = col;
+	iniy = y = row;
+}
+void Pacman::Render() {
+	SDL_Rect destRect;
+	destRect.x = x; destRect.y = y;
+	texture->renderFrame(game->renderer,destRect,dir,11);
 }
--- Pacman/Pacman.h
+++ Pacman/Pacman.h
@@ -1,8 +1,29 @@
 #pragma once
+#include "Texture.h"
+#include "Game.h"
+
+using namespace std;
+
 class Pacman
 {
+private:
+	unsigned int x = 0;//Position X
+	unsigned int y = 0;// Position Y
+	unsigned int inix = 0;//Start Position X
+	unsigned int iniy = 0;//Start Position Y
+	unsigned int dir = 0;//Movement direction
+	unsigned int dirbuffer = 0;//Movement direction buffer
+	unsigned int lives = 3; //Numberof remaining lifes
+	unsigned int energy = 0;//Remaining energy
+	Texture* texture = nullptr;
+	Game* game = nullptr;
 public:
-	Pacman();
+	Pacman(Game* a, Texture* b);
 	~Pacman();
+	void setPos(unsigned int row, unsigned int col);
+	void Render();
+	void Update();
+	void Die();
+
 };
 
