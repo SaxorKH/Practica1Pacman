923068145b5482973abaf34295fe468306b7cbdd
SaxorKH <digicarlos@live.com>
2017-12-20 15:55:57 +0100

    Añadidos algunos comentarios

--- Pacman/GameObject.cpp
+++ Pacman/GameObject.cpp
@@ -1,12 +1,12 @@
 #include "GameObject.h"
 #include "Game.h"
-
+//Se nombran todos los métodos que tendran los diferentes tipos de GameObjects que serán especificados en cadatipo posteriormente
 
 
 GameObject::GameObject()
 {
 }
-
+//Se crea el puntero del GameObject al Game
 GameObject::GameObject(Game * game)
 {
 	this->game = game;
